f048d3c9fe53c177433395f856d32d54b1617f22
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-02-18 12:50:56 +0100

    First Push

---
+++ classes.py
@@ -0,0 +1,104 @@
+# device records
+import os
+
+
+class DeviceRecord:
+
+    def __init__(self):
+        pass
+
+    device_id = ""
+    family = ""
+    model = ""
+    serial = ""
+    firmware_version = ""
+    capacity = ""
+    sector_sizes = ""
+    rotation_rate = ""
+    device_is = ""
+    ata_version = ""
+    sata_version = ""
+    smart_support_available = False
+    smart_support_enabled = False
+
+
+# HDD/SSD class
+def run_smartctl(str_args):
+    cmdstring = "smartctl " + str_args
+    the_output = os.popen(cmdstring).read()
+    lines = str.splitlines(the_output)
+    # lines = ["/dev/sda"]
+    return lines
+
+
+class MassStorageDevice:
+    dev = None
+    device_id = None
+
+    def __init__(self, device_id):
+        self.device_id = device_id
+        self.dev = self.get_device_info()
+
+    def get_device_info(self):
+        dev = DeviceRecord()
+        dev_info_lines = run_smartctl("-i " + self.device_id)
+        b_entered_info_section = False
+        dev.device_id = self
+        for line2 in dev_info_lines:
+            if not b_entered_info_section:
+                line2.split(" ", 2)
+                # if (the_first_field[0].lower() == 'smartctl' ):
+                #    print "SmartCtl Version is: " + the_first_field[1]
+                if line2.lower() == "=== start of information section ===":
+                    b_entered_info_section = True
+            else:
+                field = line2.split(":", 1)
+                if field[0].lower() == "model family":
+                    dev.family = field[1].strip()
+                elif field[0].lower() == "device model":
+                    dev.model = field[1].strip()
+                elif field[0].lower() == "serial number":
+                    dev.serial = field[1].strip()
+                elif field[0].lower() == "firmware version":
+                    dev.firmware_version = field[1].strip()
+                elif field[0].lower() == "user capacity":
+                    dev.capacity = field[1].strip()
+                elif field[0].lower() == "sector sizes":
+                    dev.sector_sizes = field[1].strip()
+                elif field[0].lower() == "rotation rate":
+                    dev.rotation_rate = field[1].strip()
+                elif field[0].lower() == "device is":
+                    dev.device_is = field[1].strip()
+                elif field[0].lower() == "ata version is":
+                    dev.ata_version = field[1].strip()
+                elif field[0].lower() == "sata version is":
+                    dev.sata_version = field[1].strip()
+                elif field[0].lower() == "smart support is":
+                    temp = field[1].strip().split(" ", 1)
+                    # temp = string.split(field[1]," ",1)
+                    str_temp = temp[0].strip().lower()
+                    if str_temp == "available":
+                        dev.smart_support_available = True
+                    elif str_temp == "unavailable":
+                        dev.smart_support_available = False
+                        dev.smart_support_enabled = False
+                    elif str_temp == "enabled":
+                        dev.smart_support_enabled = True
+                    elif str_temp == "disabled":
+                        dev.smart_support_enabled = False
+        return dev
+
+    def get_temperature(self):
+        global current_temp
+        dev_info_lines = run_smartctl("-l scttemp " + self.device_id)
+        for line2 in dev_info_lines:
+            line2.split(" ", 2)
+            field = line2.split(":", 1)
+            current_temp = 20  # default value
+            if field[0].lower() == "current temperature":
+                current_temp = field[1].strip()
+            # elif  (field[0].lower() == "device model" ):
+            #    dev.model = field[1].strip()
+            # elif  (field[0].lower() == "serial number" ):
+            #    dev.serial = field[1].strip()
+        return current_temp
---
+++ main.py
@@ -0,0 +1,130 @@
+import _thread
+
+import pigpio
+import psutil
+import time
+
+import classes
+
+
+def get_device_ids():
+    lines = classes.run_smartctl("--scan")
+    device_list = []
+    for line in lines:
+        device_id = line.split(" ", 1)[0]
+        device_list.append(device_id)
+    return device_list
+
+
+def remove_dupes(devices):
+    newlist = []
+    for device in devices:
+        currentSerial = device.dev.serial
+        present = False
+        for item in newlist:
+            if item.dev.serial == currentSerial:
+                present = True
+        if not present:
+            newlist.append(device)
+    return newlist
+
+
+def remove_disabled_unsupported(devices):
+    newlist = []
+    for device in devices:
+        if device.dev.smart_support_available and device.dev.smart_support_enabled:
+            newlist.append(device)
+    return newlist
+
+
+def compute_device_fan_speed(devices):
+    max_temp = 0
+    for device in devices:
+        current_temp = device.get_temperature()
+        if current_temp > max_temp:
+            max_temp = current_temp
+
+    if max_temp <= 30:
+        return 0
+    elif 30 < max_temp <= 45:
+        return ((max_temp - 30) / 15) * 40
+    elif 45 < max_temp <= 55:
+        return ((max_temp - 45) / 10) * 60 + 40
+    else:
+        return 100
+
+
+def compute_cpu_fan_speed():
+    cpu_temp = psutil.CPUTemperature()
+    cpu_usage = psutil.cpu_percent()
+
+    if cpu_temp <= 30:
+        return 0
+    elif 30 < cpu_temp <= 45:
+        return ((cpu_temp - 30) / 15) * 25 + (cpu_usage / 100) * 75
+    elif 45 < cpu_temp <= 60:
+        return ((cpu_temp - 45) / 15) * 35 + 25 + (cpu_usage / 100) * 40
+    elif 60 < cpu_temp <= 85:
+        return ((cpu_temp - 60) / 25) * 40 + 60
+    else:
+        return 100
+
+
+def fan_speed_control(list_of_devices):
+    pwm_frequency = 25000
+    fan_pin = 12
+    rpm_pin = 6
+    threshold = 10
+    sleep_time = 0.2  # s
+
+    pigpio.pi.set_PWM_range(fan_pin, 100)
+    pigpio.pi.set_PWM_frequency(pwm_frequency)
+    pigpio.pi.set_PWM_dutycycle(fan_pin, 0)
+
+    prec_duty_cicle = 0
+
+    # main cicle
+    while True:
+        current_rpm = measure_rpm(rpm_pin)
+        new_duty_cicle = max(compute_cpu_fan_speed(), compute_device_fan_speed(list_of_devices))
+        print(
+            "Current RPM: " + str(current_rpm) + " - Current Duty Cicle: " + str(
+                prec_duty_cicle) + " - New Duty Cicle: " + str(new_duty_cicle))
+        if new_duty_cicle - prec_duty_cicle > threshold:
+            pigpio.pi.set_PWM_dutycycle(fan_pin, new_duty_cicle)
+            prec_duty_cicle = new_duty_cicle
+        time.sleep(sleep_time)
+
+
+def measure_rpm(rpm_pin):
+    num_cicles = 10
+
+    start = time.time()
+    for impulse_count in range(num_cicles):
+        pigpio.pi.wait_for_edge(rpm_pin, pigpio.RISING_EDGE)
+
+    duration = time.time() - start  # seconds to run for loop
+    frequency = num_cicles / duration  # in Hz
+    return int((frequency * 60) / 2)
+
+
+def main():
+    device_list = get_device_ids()
+
+    if not device_list:
+        print("No mass storage devices found.")
+
+    list_of_devices = []
+
+    for str_device in device_list:
+        device = classes.MassStorageDevice(str_device)
+        print("DEVICE FOUND: " + device.dev)
+        list_of_devices.append(device)
+
+    list_of_devices = remove_dupes(list_of_devices)
+    list_of_devices = remove_disabled_unsupported(list_of_devices)
+
+    _thread.start_new_thread(fan_speed_control, (list_of_devices,))
+
+# program
+main()
